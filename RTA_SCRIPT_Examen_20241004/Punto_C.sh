ef9707ee10f4b57ec6e3af364e7e1cb4ac9300d1
ThiagooTomass <thiagotomasramos@gmail.com>
2024-10-05 15:47:38 +0000

    ADD: permisos

--- RTA_SCRIPT_Examen_20241004/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_C.sh
@@ -40,5 +40,13 @@ done
 IFS=$ANT_IFS
 
 
+#PERMISOS
 
-
+sudo chown -R p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno_1
+sudo chmod -R 750 /Examenes-UTN/alumno_1
+sudo chown -R p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/alumno_2
+sudo chmod -R 760 /Examenes-UTN/alumno_2
+sudo chown -R p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chmod -R 700 /Examenes-UTN/alumno_3
+sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores/
+sudo chmod -R 775 /Examenes-UTN/profesores/
